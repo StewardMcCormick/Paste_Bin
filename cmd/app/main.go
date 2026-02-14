49a1b79eb1440a64af97a2b78acee2670c389fd7
Egor Bessonov <bessonoven@mail.ru>
2026-02-14 11:16:43 +0300

    Add configuration

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"context"
+	"github.com/StewardMcCormick/Paste_Bin/config"
+)
+
 func main() {
+	cfg, err := config.InitConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	RunApp(context.Background(), cfg)
+}
+
+func RunApp(ctx context.Context, cfg *config.Config) {
 
 }
--- config/config.go
+++ config/config.go
@@ -1,1 +1,36 @@
 package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
+
+type App struct {
+	Name    string `yaml:"name" env-required:"true"`
+	Version string `yaml:"version" env-required:"true"`
+}
+
+type Server struct {
+	Host string `env:"SERVER_HOST" env-default:"localhost"`
+	Port string `env:"SERVER_PORT" env-default:"80"`
+}
+
+type Config struct {
+	App    App    `yaml:"app"`
+	Server Server `yaml:"server"`
+}
+
+func InitConfig() (*Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := &Config{}
+	err = cleanenv.ReadConfig("config.yaml", cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
