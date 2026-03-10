7a019ee1690165b5b36c7120282ec0a35b7a790f
Egor Bessonov <bessonoven@mail.ru>
2026-03-10 22:00:25 +0300

    Add tests for Auth middleware

---
+++ internal/handler/middleware/auth_test.go
@@ -0,0 +1,134 @@
+package middleware
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	errs "github.com/StewardMcCormick/Paste_Bin/internal/error"
+	"github.com/StewardMcCormick/Paste_Bin/internal/handler/middleware/mocks"
+	appctx "github.com/StewardMcCormick/Paste_Bin/internal/util/app_context"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+type AuthMiddlewareTestSuite struct {
+	suite.Suite
+	auth       *mocks.MockAuthService
+	middleware Auth
+}
+
+func TestAuthMiddlewareSuite(t *testing.T) {
+	suite.Run(t, new(AuthMiddlewareTestSuite))
+}
+
+func (s *AuthMiddlewareTestSuite) SetupTest() {
+	s.auth = mocks.NewMockAuthService(s.T())
+
+	s.middleware = NewAuth(s.auth)
+}
+
+func (s *AuthMiddlewareTestSuite) getTestHandler(body []byte) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		if body != nil {
+			w.Write(body)
+		}
+	})
+}
+
+func (s *AuthMiddlewareTestSuite) Test_Auth_Success() {
+	validApiKey := "pb_test_abcd_acbdefghijkl"
+	body := []byte(`{"message": "hello"`)
+
+	var capturedContext context.Context
+	testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		capturedContext = r.Context()
+		w.WriteHeader(http.StatusOK)
+		w.Write(body)
+	})
+
+	s.auth.EXPECT().
+		Authenticate(mock.Anything, mock.Anything).
+		Return(1, nil).
+		Once()
+
+	req := httptest.NewRequest("GET", "/", nil)
+
+	req.Header.Set(APIKeyHeader, validApiKey)
+	w := httptest.NewRecorder()
+
+	midd := s.middleware.Handler(testHandler)
+
+	midd.ServeHTTP(w, req)
+
+	resultBody, err := io.ReadAll(w.Result().Body)
+	s.Require().NoError(err)
+	userIdFromCtx, err := appctx.GetUserId(capturedContext)
+	s.Require().NoError(err)
+
+	s.Equal(http.StatusOK, w.Result().StatusCode)
+	s.Equal(string(body), string(resultBody))
+	s.Equal(int64(1), userIdFromCtx)
+}
+
+func (s *AuthMiddlewareTestSuite) Test_Auth_Error() {
+	cases := []struct {
+		name       string
+		setup      func()
+		value      string
+		wantStatus int
+	}{
+		{
+			"Invalid API-Key",
+			func() {
+			},
+			"invalid-key",
+			http.StatusUnauthorized,
+		},
+		{
+			"Auth Service Error - Unauthorized",
+			func() {
+				s.auth.EXPECT().
+					Authenticate(mock.Anything, mock.Anything).
+					Return(0, errs.Unauthorized).
+					Once()
+			},
+			"pb_test_abcd_acbdefghijkl",
+			http.StatusUnauthorized,
+		},
+		{
+			"Auth Service Error - Internal Error",
+			func() {
+				s.auth.EXPECT().
+					Authenticate(mock.Anything, mock.Anything).
+					Return(0, errs.InternalError).
+					Once()
+			},
+			"pb_test_abcd_acbdefghijkl",
+			http.StatusInternalServerError,
+		},
+	}
+
+	for _, tc := range cases {
+		s.Run(tc.name, func() {
+			s.SetupTest()
+			tc.setup()
+
+			testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("hello"))
+			})
+
+			req := httptest.NewRequest("GET", "/", nil)
+			req.Header.Set(APIKeyHeader, tc.value)
+			w := httptest.NewRecorder()
+
+			midd := s.middleware.Handler(testHandler)
+			midd.ServeHTTP(w, req)
+
+			s.Equal(tc.wantStatus, w.Result().StatusCode)
+		})
+	}
+}
